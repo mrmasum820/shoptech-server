2ec6f96bc61d013efdec0c0c349a870a603e824b
MR Masum <mrmasum820@gmail.com>
2023-12-06 23:18:53 +0600

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzjopzw.mongodb.net/?retryWrites=true&w=majority`
 // console.log(uri);
 
@@ -34,7 +34,7 @@ async function run() {
             res.send({ status: true, data: product });
         })
 
-        app.post("/product", async (req, res) => {
+        app.post("/products", async (req, res) => {
             const product = req.body;
 
             const result = await productCollection.insertOne(product);
@@ -42,10 +42,10 @@ async function run() {
             res.send(result);
         });
 
-        app.delete("/product/:id", async (req, res) => {
+        app.delete("/products/:id", async (req, res) => {
             const id = req.params.id;
 
-            const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+            const result = await productCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
         });
 
